2e474d3e51141ed6a55f3dc8d67f4cf44e5a297b
ccbc7 <flexibleelbow@gmail.com>
2023-05-29 20:22:51 +0900

    ➕ ecsデプロイのためのcopilote環境ファイルの追加と、rubocopでインデント等を修正。

--- backend/app/controllers/api/v1/notes_controller.rb
+++ backend/app/controllers/api/v1/notes_controller.rb
@@ -1,32 +1,37 @@
-class Api::V1::NotesController < ApplicationController
-  def index
-    @notes = Note.all
+module Api
+  module V1
+    class NotesController < ApplicationController
+      def index
+        @notes = Note.all
 
-    render json: @notes
-  end
+        render json: @notes
+      end
 
-  def create
-    @note = Note.new(note_params)
+      def create
+        @note = Note.new(note_params)
 
-    if @note.save
-      render json: @note, status: :created
-    else
-      render json: @note.errors, status: :unprocessable_entity
+        if @note.save
+          render json: @note, status: :created
+        else
+          render json: @note.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @note = Note.find(params[:id])
+        @note.destroy
+        head :no_content # 削除が成功した場合、HTTPステータスコード204を返します。
+      end
+
+      private
+
+      def set_note
+        @note = Note.find(params[:id])
+      end
+
+      def note_params
+        params.require(:note).permit(:title, :content)
+      end
     end
   end
-
-  def destroy
-    @note = Note.find(params[:id])
-    @note.destroy
-    head :no_content # 削除が成功した場合、HTTPステータスコード204を返します。
-  end
-
-  private
-    def set_note
-      @note = Note.find(params[:id])
-    end
-
-    def note_params
-      params.require(:note).permit(:title, :content)
-    end
 end
