e344a3a673671c1f80807dc57ac810852f3ba3dd
ccbc7 <flexibleelbow@gmail.com>
2023-06-05 18:20:20 +0900

    ✨ FIREBASEを使った認証機能に基づいたコントローラの変更です。

--- backend/app/controllers/api/v1/notes_controller.rb
+++ backend/app/controllers/api/v1/notes_controller.rb
@@ -2,13 +2,18 @@ module Api
   module V1
     class NotesController < ApplicationController
       def index
-        @notes = Note.all
+        if params[:user_id]
+          @notes = Note.where(user_id: params[:user_id])
+        else
+          @notes = Note.all
+        end
 
         render json: @notes
       end
 
       def create
         @note = Note.new(note_params)
+        @note.user_id = params[:user_id]  # リクエストから userId を取得して user_id をセット
 
         if @note.save
           render json: @note, status: :created
@@ -19,8 +24,12 @@ module Api
 
       def destroy
         @note = Note.find(params[:id])
-        @note.destroy
-        head :no_content
+        if @note.user_id == params[:user_id]
+          @note.destroy
+          head :no_content
+        else
+          render json: { error: 'Unauthorized' }, status: :unauthorized
+        end
       end
 
       private
@@ -30,7 +39,7 @@ module Api
       end
 
       def note_params
-        params.require(:note).permit(:title, :content)
+        params.require(:note).permit(:title, :content, :user_id)
       end
     end
   end
