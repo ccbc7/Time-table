f8b59682dfba8cf0415d7dc2e4985ecf2d39e0df
ccbc7 <flexibleelbow@gmail.com>
2023-06-08 22:26:08 +0900

    ⚡️ 🚧 ヘッダーメニューを見やすくしています。

--- frontend/app/src/components/Header.jsx
+++ frontend/app/src/components/Header.jsx
@@ -2,26 +2,24 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-// import UserInfo from "@/components/UserInfo";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../utils/firebase";
 
-// const user = {
-//   name: "Tom Cook",
-//   email: "tom@example.com",
-//   imageUrl:
-//     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-// };
 const navigation = [
   { name: "本日の予定", href: "#", current: false },
   { name: "予約", href: "#", current: false },
   { name: "予定確認", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
+  { name: "施設登録", href: "#", current: false },
 ];
 const userNavigation = [
   { name: "プロフィール", href: "#" },
   { name: "アカウント設定", href: "#" },
   { name: "サインアウト", href: "#" },
+];
+
+const guestNavigation = [
+  { name: "新規登録", href: "/SignUp" }, // 更新: 実際の登録ページへのリンクに置き換えてください
+  { name: "サインイン", href: "/SignIn" }, // 更新: 実際のログインページへのリンクに置き換えてください
 ];
 
 function classNames(...classes) {
@@ -32,15 +30,6 @@ export default function Example() {
   const [user] = useAuthState(auth);
   return (
     <>
-      {/* <UserInfo /> */}
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-full">
         <Disclosure as="nav" className="bg-lime-50">
           {({ open }) => (
@@ -83,57 +72,92 @@ export default function Example() {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
+                      {user ? (
+                        <>
+                          <>
+                            <button
+                              type="button"
+                              className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                            >
+                              <span className="sr-only">
+                                View notifications
+                              </span>
+                              <BellIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </button>
 
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-8 w-8 rounded-full"
-                              // src={user.imageUrl}
-                              src={
-                                user ? user.photoURL : "/default_profile2.png"
-                              }
-                              alt=""
-                            />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
+                            {/* Profile dropdown */}
+                            <Menu as="div" className="relative ml-3">
+                              <div>
+                                <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                  <span className="sr-only">
+                                    Open user menu
+                                  </span>
+                                  <img
+                                    className="h-8 w-8 rounded-full"
+                                    // src={user.imageUrl}
+                                    src={
+                                      user
+                                        ? user.photoURL
+                                        : "/default_profile2.png"
+                                    }
+                                    alt=""
+                                  />
+                                </Menu.Button>
+                              </div>
+
+                              <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                              >
+                                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                  {userNavigation.map((item) => (
+                                    <Menu.Item key={item.name}>
+                                      {({ active }) => (
+                                        <a
+                                          href={item.href}
+                                          className={classNames(
+                                            active ? "bg-gray-100" : "",
+                                            "block px-4 py-2 text-sm text-gray-700"
+                                          )}
+                                          onClick={
+                                            item.name === "サインアウト"
+                                              ? () => auth.signOut()
+                                              : null
+                                          }
+                                        >
+                                          {item.name}
+                                        </a>
+                                      )}
+                                    </Menu.Item>
+                                  ))}
+                                </Menu.Items>
+                              </Transition>
+                            </Menu>
+                          </>
+                        </>
+                      ) : (
+                        <>
+                          {guestNavigation.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className="text-gray-900 hover:bg-gray-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                            >
+                              {item.name}
+                            </a>
+                          ))}
+                        </>
+                      )}
+
+
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
@@ -207,6 +231,11 @@ export default function Example() {
                         as="a"
                         href={item.href}
                         className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                        onClick={
+                          item.name === "サインアウト"
+                            ? () => auth.signOut()
+                            : null
+                        }
                       >
                         {item.name}
                       </Disclosure.Button>
@@ -217,12 +246,6 @@ export default function Example() {
             </>
           )}
         </Disclosure>
-
-        {/* <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            Your content
-          </div>
-        </main> */}
       </div>
     </>
   );
