45793d5dc7a974e72b9d20dac275fd143983aadd
ccbc7 <flexibleelbow@gmail.com>
2023-06-17 16:41:21 +0900

    🚧 create アクションのための型です。

--- frontend/app/src/pages/pictures/create.jsx
+++ frontend/app/src/pages/pictures/create.jsx
@@ -1,68 +1,106 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Image from "next/image";
+import Header from "@/components/Header";
+import { auth } from "@/utils/firebase";
 
 function Create() {
   const [selectedFile, setSelectedFile] = useState();
   const [uploadedImage, setUploadedImage] = useState();
   const [images, setImages] = useState([]);
+  const [userId, setUserId] = useState();
 
   useEffect(() => {
     axios.get("/pictures").then((response) => {
-      const imagesData = response.data.map((picture) => picture.image_url);
-      setImages(imagesData);
+      setImages(response.data);
     });
   }, []);
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserId(user.uid);
+      } else {
+        console.log("User is not logged in.");
+      }
+    });
+
+    // Clean up subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
   const fileSelectedHandler = (event) => {
-    setSelectedFile(event.target.files[0]); // event.target.files[0] は選択したファイル
+    setSelectedFile(event.target.files[0]);
   };
 
   const fileUploadHandler = () => {
-    const fd = new FormData(); // FormData はフォームデータを扱うためのクラス
+    const fd = new FormData();
     fd.append("picture[image]", selectedFile, selectedFile.name);
-    axios.post("/pictures", fd).then((res) => {
-      console.log(res);
-      setUploadedImage(res.data.image_url); // アップロードした写真のURLを保存します。
-      setImages([...images, res.data.image_url]); // アップロードした画像をimages配列に追加します。
+
+    // ユーザーIDをFormDataに追加
+    if (userId) {
+      fd.append("user_id", userId);
+
+      axios.post("/pictures", fd).then((res) => {
+        console.log(res);
+        setUploadedImage(res.data.image_url);
+        setImages([...images, res.data]);
+      });
+    } else {
+      console.log("User is not logged in.");
+    }
+  };
+
+  const deleteImageHandler = (id) => {
+    axios.delete(`/pictures/${id}`).then(() => {
+      setImages(images.filter((image) => image.id !== id));
     });
   };
 
   return (
-    <div>
-      <input type="file" onChange={fileSelectedHandler} />
-      <button
-        className="bg-blue-500 ml-4 px-3 hover:ring-4 ring-indigo-300 rounded-md"
-        onClick={fileUploadHandler}
-      >
-        Upload
-      </button>
-      {uploadedImage && (
-        <div>
-          <h2>Uploaded Image</h2>
-          <img
-            className="w-16 h-16 object-cover"
-            src={uploadedImage}
-            alt="Uploaded"
-          />
-        </div>
-      )}
-      {images.length > 0 && ( // images 配列の要素数が 0 より大きい場合にのみ、画像を表示します。
-        <div>
-          <h2>All Uploaded Images</h2>
-          <div className="flex  flex-wrap">
-            {images.map((imageUrl, index) => (
-              <img
-                className="w-16 h-16 object-cover mx-2 my-2"
-                key={index}
-                src={imageUrl}
-                alt={`Uploaded ${index}`}
-              />
-            ))}
+    <>
+      <Header />
+      <p>User ID: {userId}</p>
+      <div>
+        <input type="file" onChange={fileSelectedHandler} />
+        <button
+          className="bg-blue-500 ml-4 px-3 hover:ring-4 ring-indigo-300 rounded-md"
+          onClick={fileUploadHandler}
+        >
+          Upload
+        </button>
+        {uploadedImage && (
+          <div>
+            <h2>Uploaded Image</h2>
+            <img
+              className="w-16 h-16 object-cover"
+              src={uploadedImage}
+              alt="Uploaded"
+            />
           </div>
-        </div>
-      )}
-    </div>
+        )}
+        {images.length > 0 && (
+          <div>
+            <h2>All Uploaded Images</h2>
+            <div className="flex flex-wrap">
+              {images.map((image, index) => (
+                <div key={index}>
+                  <img
+                    className="w-16 h-16 object-cover mx-2 my-2"
+                    src={image.image_url}
+                    alt={`Uploaded ${index}`}
+                  />
+                  <p>{image.id}</p>
+                  <p>{image.user_id}</p>
+                  <button onClick={() => deleteImageHandler(image.id)}>
+                    Delete
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
