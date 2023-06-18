92d2cc6e42ff489fcf6aa88e904819d33bad5c3b
ccbc7 <flexibleelbow@gmail.com>
2023-06-18 14:10:58 +0900

    ✅ テストを追加しました。

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
     firebase_uid { "MyString" }
+      sequence(:username) { |n| "test_user#{n}" } # ユニークなユーザー名を生成するためのシーケンス
+      sequence(:user_id) { |n| n } # ユニークなユーザーIDを生成するためのシーケンス
+      bio { "This is a bio" }
   end
 end
