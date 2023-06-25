9e29362b4cbfceea3dac705ccb8325f1bbd637e8
ccbc7 <flexibleelbow@gmail.com>
2023-06-25 15:02:39 +0900

    ✨ 使用目的を登録できるようにしました。

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_103958) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_113959) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -79,14 +79,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_103958) do
   end
 
   create_table "reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.bigint "location_id", null: false
     t.string "period"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "booked_user_id"
+    t.string "user_id"
+    t.string "facility_user_name"
+    t.string "purpose"
     t.index ["location_id"], name: "index_reservations_on_location_id"
-    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -101,5 +103,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_103958) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "icons", "users"
   add_foreign_key "reservations", "locations"
-  add_foreign_key "reservations", "users"
 end
