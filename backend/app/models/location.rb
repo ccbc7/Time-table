e34addc0eae2853e8784976800f9d9e014decf67
ccbc7 <flexibleelbow@gmail.com>
2023-06-25 14:58:51 +0900

    ⚡️ S3で扱う画像を表示するための処理です。

--- backend/app/models/location.rb
+++ backend/app/models/location.rb
@@ -3,4 +3,16 @@ class Location < ApplicationRecord
   # belongs_to :user, foreign_key: 'user_id'
   belongs_to :user, foreign_key: 'user_id', primary_key: 'user_id'
   has_many :reservations
+
+  def image_url
+    if image.attached?
+      Rails.application.routes.url_helpers.url_for(image)
+    else
+      nil
+    end
+  end
+  
+  def as_json_with_image_url
+    as_json.merge(image_url: image_url)
+  end
 end
