a73ab5814e4852b95523b3314deb0c1adbfc7ba9
ccbc7 <flexibleelbow@gmail.com>
2023-06-25 15:01:13 +0900

    ✨ reservationモデルのlocation_idからlocationモデルにアクセスできます

--- backend/app/models/reservation.rb
+++ backend/app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, primary_key: 'user_id'
   belongs_to :location
+
+  def as_json(options = {})
+    super(options.merge(include: :location))
+    super(options.merge(include: :user))
+  end
 end
