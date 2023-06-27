eff88995ca6b128b094baf318e0a0da97007e20e
ccbc7 <flexibleelbow@gmail.com>
2023-06-27 17:53:33 +0900

    ⚡️ 送信モーダルを追加しました。

--- frontend/app/src/pages/ReservationCreate/[id].jsx
+++ frontend/app/src/pages/ReservationCreate/[id].jsx
@@ -4,15 +4,108 @@ import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import Header from "@/components/Header";
 import { auth } from "@/utils/firebase";
-import Timetable from "../Timetable";
+import Modal from "@/components/Modal";
+
+function toDateString(date) {
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+    2,
+    "0"
+  )}-${String(date.getDate()).padStart(2, "0")}`;
+}
 
 const ReservationCreate = () => {
   const router = useRouter();
-  const { id } = router.query; // location idをURLパラメータから取得
-  const { register, handleSubmit, errors } = useForm();
+  const { id } = router.query;
+  const today = new Date();
+  const todayString = toDateString(today);
+  const [reservationsMap, setReservationsMap] = useState({});
+  const [weekOffset, setWeekOffset] = useState(0);
   const [location, setLocation] = useState(null);
+  const [reservations, setReservations] = useState([]);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const selectedDate = watch("date"); // 日付フィールドの値を監視
+  const selectedPeriod = watch("period"); // 時限フィールドの値を監視
+  const [showModal, setShowModal] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
-  // locationを取得
+  useEffect(() => {
+    const fetchReservations = async () => {
+      const response = await axios.get("/reservations");
+      setReservations(response.data);
+    };
+    fetchReservations();
+  }, []);
+
+  const hours = [
+    "朝",
+    "1時間目",
+    "2時間目",
+    "3時間目",
+    "4時間目",
+    "昼休み",
+    "5時間目",
+    "6時間目",
+    "放課後",
+  ];
+
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const reservationsResponse = await axios.get(
+        `/reservations?location_id=${id}`
+      );
+      setReservations(reservationsResponse.data);
+
+      const reservationsMap = {};
+      for (const reservation of reservationsResponse.data) {
+        const date = toDateString(new Date(reservation.date));
+        const hour = reservation.period;
+        const key = `${date}-${hour}`;
+
+        reservationsMap[key] = reservation;
+      }
+      setReservationsMap(reservationsMap);
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  useEffect(() => {
+    const today = new Date();
+    today.setDate(today.getDate() + weekOffset * 7);
+    const firstDayOfWeek =
+      today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1);
+
+    const newDays = Array(7)
+      .fill(0)
+      .map((_, i) => {
+        const thisDate = new Date(today);
+        thisDate.setDate(firstDayOfWeek + i);
+        return {
+          date: thisDate,
+          name: ["日", "月", "火", "水", "木", "金", "土"][thisDate.getDay()],
+        };
+      });
+    setDays(newDays);
+  }, [weekOffset]);
+
+  useEffect(() => {
+    const fetchReservations = async () => {
+      const response = await axios.get("/reservations");
+      setReservations(response.data);
+    };
+
+    fetchReservations();
+  }, []);
+
   useEffect(() => {
     const fetchLocation = async () => {
       const response = await axios.get(`/locations/${id}`);
@@ -24,37 +117,21 @@ const ReservationCreate = () => {
     }
   }, [id]);
 
-  // フォームを送信
-  const onSubmit = async (data) => {
-    if (!auth.currentUser) {
-      alert("サインインしてください。");
-      return;
-    }
-
+  const createBooking = async (data, date) => {
     const fd = new FormData();
     fd.append("reservation[period]", data.period);
-    fd.append("reservation[date]", data.date);
-    // fd.append("reservation[booked_user_id]", auth.currentUser.uid);
-    fd.append("reservation[location_id]", id); //
+    fd.append("reservation[date]", date.toISOString().split("T")[0]);
+    fd.append("reservation[facility_user_name]", data.facility_user_name);
+    fd.append("reservation[location_id]", id);
+    fd.append("reservation[purpose]", data.purpose);
     fd.append("reservation[user_id]", auth.currentUser.uid);
 
-    // await axios.post("/reservations", fd);
-    // router.push(`/reservations`);
-
-    //   await axios.post("/reservations", fd, {
-    //     headers: {
-    //       "Content-Type": "multipart/form-data",
-    //     },
-    //   });
-    //   router.push(`/reservations`);
-    // };
     try {
       await axios.post("/reservations", fd, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-      router.push(`/ReservationIndex`);
     } catch (err) {
       if (err.response) {
         console.log("Error response data: ", err.response.data);
@@ -66,19 +143,122 @@ const ReservationCreate = () => {
     }
   };
 
+  const onSubmit = async (data) => {
+    if (!auth.currentUser) {
+      alert("サインインしてください。");
+      return;
+    }
+
+    if (data.weeklyBooking) {
+      let date = new Date(data.date);
+      const endDate = new Date(date.getFullYear() + 1, 2, 31);
+      while (date <= endDate) {
+        await createBooking(data, date);
+        date.setDate(date.getDate() + 7);
+      }
+    } else {
+      await createBooking(data, new Date(data.date));
+    }
+
+    setSubmitted(true);
+    setShowModal(true);
+
+    setTimeout(() => {
+      setShowModal(false);
+      router.push(`/ReservationIndex`);
+    }, 1000);
+  };
+
   if (!location) return null;
 
   return (
     <>
       <Header />
-      {/* <Timetable /> */}
-      <div className="border-2 min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md -mt-32">
+      {submitted && (
+        <Modal open={showModal} onClose={() => setShowModal(false)} />
+      )}
+      <div className="border-2 min-h-screen bg-gray-100 flex items-start justify-center py-3">
+        <div className="bg-white p-2 rounded-lg shadow-lg w-full max-w-3xl">
           <h2 className="text-center text-3xl mb-2">
             予約作成: {location.location_name}
           </h2>
-          <img src={location.image_url} alt={location.location_name} />
+          <table className="table-fixed bg-white rounded-md overflow-hidden w-full">
+            <thead>
+              <tr className="bg-lime-50 border">
+                <th className="p-2 w-20">時限</th>
+                {days.map((day, index) => (
+                  <th key={index} className="p-2 border w-20">
+                    {day.date.getMonth() + 1}/{day.date.getDate()}
+                    {day.name}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="border">
+              {hours.map((hour, index) => (
+                <tr key={index}>
+                  <td className="p-2 border w-20">{hour}</td>
+                  {days.map((day, dayIndex) => {
+                    const date = toDateString(day.date);
+                    const key = `${date}-${hour}`;
+                    const reservation = reservationsMap[key];
+                    const reservationExists = reservation !== undefined;
+
+                    return (
+                      <td
+                        key={index}
+                        className={`p-2 border ${
+                          reservationExists
+                            ? "bg-red-50" // 予約がある場合の背景色
+                            : toDateString(day.date) === selectedDate &&
+                              hour === selectedPeriod
+                            ? "bg-green-100" // 選択された日付と時限の場合の背景色
+                            : toDateString(day.date) === todayString
+                            ? "bg-cyan-50"
+                            : ""
+                        }`}
+                      >
+                        {reservationExists
+                          ? reservation.facility_user_name
+                          : "---"}
+                      </td>
+                    );
+                  })}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex justify-center mb-5 mt-3 border-b pb-3">
+            <button
+              onClick={() => setWeekOffset(weekOffset - 1)}
+              className="mr-8 px-5 py-2 bg-blue-500 text-white rounded-md"
+            >
+              前の週
+            </button>
+            <h3 className="text-xl">{`${today.getFullYear()}年${
+              today.getMonth() + 1
+            }月${today.getDate()}日`}</h3>
+            <button
+              onClick={() => setWeekOffset(weekOffset + 1)}
+              className="ml-8 px-5 py-2 bg-blue-500 text-white rounded-md"
+            >
+              次の週
+            </button>
+          </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex justify-center items-center mb-2">
+              <label className="pr-3 w-24 text-right">日付:</label>
+              <input
+                {...register("date", { required: "日付を入力してください" })}
+                type="date"
+                min={toDateString(new Date())}
+                className="px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md w-60 focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+              />
+            </div>
+            <div className="flex justify-center items-center mb-2">
+              <label className="pr-3 text-right">毎週予約する(3/31まで):</label>
+              <input {...register("weeklyBooking")} type="checkbox" />
+            </div>
             <div className="flex justify-center items-center mb-2">
               <label className="pr-3 w-24 text-right">時限:</label>
               <select
@@ -86,6 +266,7 @@ const ReservationCreate = () => {
                 className="px-3 py-2 border border-gray-300 rounded-md w-60 focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
               >
                 <option value="">選択してください</option>
+                <option value="1時間目">朝</option>
                 <option value="1時間目">1時間目</option>
                 <option value="2時間目">2時間目</option>
                 <option value="3時間目">3時間目</option>
@@ -97,20 +278,43 @@ const ReservationCreate = () => {
               </select>
             </div>
             <div className="flex justify-center items-center mb-2">
-              <label className="pr-3 w-24 text-right">日付:</label>
+              <label className="pr-3 w-24 text-right">使用者:</label>
               <input
-                {...register("date", { required: "日付を入力してください" })}
-                type="date"
-                min={new Date().toISOString().split("T")[0]}
+                {...register("facility_user_name", {
+                  required: "使用者を入力してください",
+                })}
+                type="text"
+                name="facility_user_name"
+                className="w-60 px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
+                placeholder="(例)3年2組"
+              />
+            </div>
+            {errors.facility_user_name && (
+              <p className="text-red-500 text-center">
+                {errors.facility_user_name.message}
+              </p>
+            )}
+            <div className="flex justify-center items-center mb-2">
+              <label className="pr-3 w-24 text-right">用途:</label>
+              <input
+                {...register("purpose", { required: "用途を入力してください" })}
+                type="text"
+                name="purpose"
+                placeholder="(例)会議"
                 className="px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md w-60 focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
               />
             </div>
+            {errors.purpose && (
+              <p className="text-red-500 text-center">
+                {errors.purpose.message}
+              </p>
+            )}
             <div className="flex justify-center items-center my-8">
               <button
                 type="submit"
-                className="px-4 py-2 bg-blue-500 text-white rounded"
+                className="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white rounded w-full"
               >
-                予約
+                予約する
               </button>
             </div>
           </form>
