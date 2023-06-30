e2c93ee1e968b7a3946ccd49caf60b540ada41a5
ccbc7 <flexibleelbow@gmail.com>
2023-07-01 04:11:04 +0900

    ✨ 画面スライドを作成するために導入しました。

--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -1,3 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+//         'gradient-conic':
+//           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,6 +31,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        'fade-out': {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 }
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in 2s ease-in forwards',
+        'fade-out': 'fade-out 2s ease-in forwards'
+      }
     },
   },
   plugins: [],
