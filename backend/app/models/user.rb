54104bd7cb822741b83455c8fc521e7dbed33855
ccbc7 <flexibleelbow@gmail.com>
2023-07-03 20:51:35 +0900

    ⬆️ 他モデルに対しての依存関係を追加しました。

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   has_one_attached :image
-  # has_many :locations, primary_key: 'user_id'
   has_many :locations, foreign_key: 'user_id', primary_key: 'user_id'
   has_many :reservations
+  has_many :notes, foreign_key: 'user_id', primary_key: 'user_id'
+  def image_url
+    return unless image.attached?
+    Rails.application.routes.url_helpers.url_for(image)
+  end
+
+  def as_json_with_image_url
+    as_json.merge(image_url: image_url)
+  end
+
+  def self.eager_load_image
+    all.with_attached_image
+  end
 end
