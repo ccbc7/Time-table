84301c899b8bb356ed1fb1b16c9b61aee143f7aa
ccbc7 <flexibleelbow@gmail.com>
2023-07-03 22:16:37 +0900

    ✨ モーダルが出現するようにしました。

--- frontend/app/src/pages/LocationCreate.jsx
+++ frontend/app/src/pages/LocationCreate.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import Header from "@/components/Header";
 import { auth } from "../utils/firebase";
+import Modal from "@/components/Modal";
 
 const LocationCreate = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -10,7 +11,9 @@ const LocationCreate = () => {
     setSelectedFile(event.target.files[0]);
   };
   const [location_name, setTitle] = useState("");
+  const [location_info, setLocationInfo] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
   const router = useRouter();
 
   const submitForm = async (e) => {
@@ -23,13 +26,15 @@ const LocationCreate = () => {
     const fd = new FormData();
     fd.append("location[location_name]", location_name);
     fd.append("location[user_id]", auth.currentUser.uid);
+    fd.append("location[location_info]", location_info);
     if (selectedFile) {
       fd.append("location[image]", selectedFile, selectedFile.name);
     }
 
     await axios.post("/locations", fd);
-
     setTitle("");
+    setLocationInfo("");
+    setSubmitted(true);
     setShowModal(true);
     setTimeout(() => {
       setShowModal(false);
@@ -40,36 +45,41 @@ const LocationCreate = () => {
   return (
     <>
       <Header />
-      {showModal && (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center">
-            <div className="inline-block bg-purple-400 rounded p-5 text-lg text-black">
-              送信しました
-            </div>
-          </div>
-        </div>
+      {submitted && (
+        <Modal open={showModal} onClose={() => setShowModal(false)} />
       )}
-      <div className="border-2 min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md -mt-32">
+      <div className="border-2 min-h-screen bg-gray-100 flex items-start justify-center py-20">
+        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
           <h2 className="text-center text-3xl mb-2">施設登録</h2>
           <form className="mt-8 space-y-6" onSubmit={submitForm}>
-            <div className="flex justify-center items-center my-8">
-                <input
-                  id="file-upload"
-                  type="file"
-                  onChange={fileSelectedHandler}
-                  className="flex justify-center items-center"
-                  placeholder="ファイルをアップロード"
-                />
+            <div className="mb-2">
+              <input
+                id="file-upload"
+                type="file"
+                onChange={fileSelectedHandler}
+                className="flex justify-center items-center"
+                placeholder="ファイルをアップロード"
+              />
             </div>
-            <div className="text-center">
-                <label className="self-center pr-3">施設名:</label>
-                <input
-                  className="px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300"
-                  placeholder="(例）会議室"
-                  value={location_name}
-                  onChange={(e) => setTitle(e.target.value)}
-                />
+            <div className="mb-2">
+              <label className="pr-3 text-right w-24 inline-block">
+                施設名:
+              </label>
+              <input
+                className="px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 w-60"
+                placeholder="(例）会議室"
+                value={location_name}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="flex items-start mb-2">
+              <label className="pr-3 text-right w-24">施設情報:</label>
+              <textarea
+                className="px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 h-40 w-60"
+                placeholder="(例)１階南側奥です。"
+                value={location_info}
+                onChange={(e) => setLocationInfo(e.target.value)}
+              />
             </div>
             <div className="flex justify-center items-center my-8">
               <button
@@ -87,4 +97,3 @@ const LocationCreate = () => {
 };
 
 export default LocationCreate;
-
