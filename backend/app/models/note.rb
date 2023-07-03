28669976d389bd1fa4c5a3f8d6384ad19146b436
ccbc7 <flexibleelbow@gmail.com>
2023-07-03 20:50:52 +0900

    ⬆️ 他モデルに対しての依存関係を導入しました。

--- backend/app/models/note.rb
+++ backend/app/models/note.rb
@@ -1,3 +1,15 @@
 class Note < ApplicationRecord
+  validates :content, :title, presence: true
+  validates :content, :title, :user_id, :location_id, presence: true
   has_one_attached :image
+  belongs_to :user, foreign_key: 'user_id', primary_key: 'user_id'
+  belongs_to :location
+
+  def as_json(options = {})
+    super(options.merge(include: [:user, :location]))
+  end
+
+  def as_json_with_user_image_url
+    as_json.merge(user: user.as_json_with_image_url)
+  end
 end
