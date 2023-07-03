fcee914891176604e1d119f376d384ace1fa93aa
ccbc7 <flexibleelbow@gmail.com>
2023-07-03 22:14:25 +0900

    ⚡️ 不要なコードを削除しました。

--- frontend/app/src/components/useRequireLogin.jsx
+++ frontend/app/src/components/useRequireLogin.jsx
@@ -2,22 +2,19 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { auth } from "@/utils/firebase";
 
-export const useRequireLogin = () => {
+export const UseRequireLogin = () => {
   const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        // ユーザーはログインしている
       } else {
-        // ユーザーはログインしていない
         router.push("/SignIn");
       }
     });
 
-    // cleanup function
     return () => unsubscribe();
   }, []);
 };
 
-export default useRequireLogin;
+export default UseRequireLogin;
