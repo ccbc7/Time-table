072c80a10d0208e7f8c4571d75cbe7af5350544f
ccbc7 <flexibleelbow@gmail.com>
2023-07-03 21:28:17 +0900

    ⚰️ 不要な記述を削除しました。

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    # sequence(:user_id) { |n| "testuser#{n}" }
     sequence(:user_id, 1000) { |n| "testuser#{n}" }
     username { "テストユーザー" }
     bio {"担当"}
