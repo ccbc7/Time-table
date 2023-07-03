1c79ef8e1087f2f8da0d9ce6ea1679b0bd784a68
ccbc7 <flexibleelbow@gmail.com>
2023-07-04 00:51:24 +0900

    🚧 vercelの警告を修正中です。

--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -1,19 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-};
+}
+
+module.exports = nextConfig
+
+
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   reactStrictMode: true,
+//   webpackDevMiddleware: (config) => {
+//     config.watchOptions = {
+//       poll: 1000,
+//       aggregateTimeout: 300,
+//     };
+//     return config;
+//   },
+// };
