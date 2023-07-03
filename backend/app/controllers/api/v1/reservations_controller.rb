8a074708604e0ee693a26742f0977b1a296d0d49
ccbc7 <flexibleelbow@gmail.com>
2023-07-03 20:46:55 +0900

    ⚡️ 予約すると、お知らせ通知するための処理を追加しました。

--- backend/app/controllers/api/v1/reservations_controller.rb
+++ backend/app/controllers/api/v1/reservations_controller.rb
@@ -1,19 +1,25 @@
 module Api
   module V1
     class ReservationsController < ApplicationController
-      before_action :set_reservation, only: [:show, :update, :destroy]
+      before_action :set_reservation, only: %i[show update destroy]
 
       def index
-        if params[:location_id]
-          @reservations = Reservation.where(location_id: params[:location_id])
-        elsif params[:user_id]
-          @reservations = Reservation.where(user_id: params[:user_id])
-        else
-          @reservations = Reservation.all
+        @reservations = if params[:location_id]
+                          Reservation.where(location_id: params[:location_id]).includes(:location, :user)
+                        elsif params[:user_id]
+                          Reservation.where(user_id: params[:user_id]).includes(:location, :user)
+                        else
+                          Reservation.all.includes(:location, :user)
+                        end
+        @reservations = @reservations.includes(:location).map do |reservation|
+          location = Location.eager_load_image.find(reservation.location_id)
+          reservation.as_json.merge(
+            location: location.as_json_with_image_url,
+            message1: location.created_at.to_s(:datetime_jp_reserve),
+            message2: "#{location.location_name}が予約されました"
+          )
         end
-        render json: @reservations.map { |reservation|
-          reservation.as_json.merge(location: reservation.location.as_json_with_image_url)
-        }
+        render json: @reservations
       end
 
       def show
