f02b11f763d6053f726b7228ec12e0d70e6582a3
ccbc7 <flexibleelbow@gmail.com>
2023-07-04 19:40:41 +0900

    🔧  本番環境で画像を扱うための処理です。

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -70,9 +70,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  Rails.application.routes.default_url_options[:host] = 'localhost'
-  Rails.application.routes.default_url_options[:port] = 3000
-  Rails.application.routes.default_url_options[:protocol] = 'http'
+  # Rails.application.routes.default_url_options[:host] = 'localhost'
+  # Rails.application.routes.default_url_options[:port] = 3000
+  # Rails.application.routes.default_url_options[:protocol] = 'http'
   config.hosts << ".time-table02.net"
-
+  Rails.application.routes.default_url_options[:host] = 'time-table02.net'
+  Rails.application.routes.default_url_options[:protocol] = 'https'
 end
