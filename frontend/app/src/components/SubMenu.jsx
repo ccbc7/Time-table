89373f8f6bc60fcdfcb31273479a2947c09e3a92
ccbc7 <flexibleelbow@gmail.com>
2023-07-17 01:04:07 +0900

    🎨 レスポンシブデザインに対応しました。

--- frontend/app/src/components/SubMenu.jsx
+++ frontend/app/src/components/SubMenu.jsx
@@ -6,19 +6,19 @@ function SubMenu() {
       <div className="border"></div>
       <div className="bg-blue-100 flex justify-center items-center py-3">
         <div className="bg-white p-2 rounded-lg shadow-lg w-full max-w-5xl">
-          <div className="flex justify-evenly">
+          <div className="flex flex-col sm:flex-row justify-evenly">
             <Link href="/locationCreate">
-              <button className="px-6 py-4 border border-gray-300 text-base font-medium rounded-full text-gray-700 bg-white hover:bg-lime-100 transition duration-200 ease-in-out shadow-md">
+              <button className="px-6 py-2 sm:py-4 border border-gray-300 text-base font-medium rounded-full text-gray-700 bg-white hover:bg-lime-100 transition duration-200 ease-in-out shadow-md mb-4 sm:mb-0  w-full">
                 施設を登録する
               </button>
             </Link>
             <Link href="/locationAll">
-              <button className="px-6 py-4 border border-gray-300 text-base font-medium rounded-full text-gray-700 bg-white hover:bg-lime-100 transition duration-200 ease-in-out shadow-md">
+              <button className="px-6 py-2 sm:py-4 border border-gray-300 text-base font-medium rounded-full text-gray-700 bg-white hover:bg-lime-100 transition duration-200 ease-in-out shadow-md mb-4 sm:mb-0  w-full">
                 施設を予約する
               </button>
             </Link>
             <Link href="/hours">
-              <button className="px-6 py-4 border border-gray-300 text-base font-medium rounded-full text-gray-700 bg-white hover:bg-violet-100 transition duration-200 ease-in-out shadow-md">
+              <button className="px-6 py-2 sm:py-4 border border-gray-300 text-base font-medium rounded-full text-gray-700 bg-white hover:bg-lime-100 transition duration-200 ease-in-out shadow-md mb-4 sm:mb-0  w-full">
                 時間設定
               </button>
             </Link>
